ac6fe9805277b7d53f35609f4cd518a707de4c04
Victor Raul <v1c70rotakmer@gmail.com>
2024-02-16 21:27:02 -0500

    feat(auth):add managment error with auth

--- src/middlewares/validatorJwt.js
+++ src/middlewares/validatorJwt.js
@@ -2,10 +2,7 @@ import jwt from 'jsonwebtoken';
 
 import { SECRET } from '../config/envs.js';
 export const validatorJwt = (req, res, next) => {
-    const accessToken =
-        req.cookies?.token ||
-        req.headers[SECRET] ||
-        req.headers.authorization.split(' ')[1];
+    const accessToken = req.cookies?.token || req.headers[SECRET] || null;
     if (!accessToken)
         return res.status(401).json({ message: 'User not authenticated' });
     try {
