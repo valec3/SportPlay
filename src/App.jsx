623fdd52186c05a6b0ac136c5d5858f8b358f54b
madays <maday.choque@gmail.com>
2024-02-16 20:53:45 -0500

    espaciado en x,y general y backgroung general

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
 	return (
 		<>
-			<div className='text-center text-primary'>App sportPlay</div>
+			<div className='bg-primary'>
+				{/* header */}
+				<main className='px-4 space-y-4'>
+					<div>App sportPlay</div>
+				</main>
+				{/* footer */}
+			</div>
 		</>
 	);
 }
