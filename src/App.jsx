2bb1b5511dbdc74426b41e8e8393435e57826607
madays <maday.choque@gmail.com>
2024-02-17 17:22:17 -0500

    test deploy2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 			<div className='bg-primary h-[100vh]'>
 				{/* header */}
 				<main className='px-4 space-y-4 text-base-100'>
-					<div className=''>App sportPlay, hola desde deploy</div>
+					<div className=''>App sportPlay, hola desde deploy 2</div>
 				</main>
 				{/* footer */}
 			</div>
