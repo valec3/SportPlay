ad7d5617ac46736ec0c868bdc9bc7a7fd69fd163
Miguel Evangelista <miguelbranagan30@gmail.com>
2024-02-18 23:28:05 -0400

    footer component in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import Footer from "./components/Footer";
+import LastMatch from "./components/homeComponents/LastMatch";
+
 function App() {
 	return (
 		<>
 			<div className='bg-primary h-[100vh]'>
 				{/* header */}
-				<main className='px-4 space-y-4 text-base-100'>
+				<main className=' px-[30px] space-y-4 text-base-100 md:px-[45px] min-h-screen'>
 					<div className=''>App sportPlay</div>
+					<LastMatch />
 				</main>
 				{/* footer */}
+				<Footer />
 			</div>
 		</>
 	);
