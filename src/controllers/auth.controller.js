4171a2689705d6f03338e47b7b6d4df33d381d1c
Victor Raul <v1c70rotakmer@gmail.com>
2024-02-19 23:43:07 -0500

    feat:(auth):Add catch for the error user exist and fix domain/cors

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,7 +7,7 @@ export const register = async (req, res) => {
         res.json({ message: 'User registered successfully' });
     } catch (error) {
         console.error('Error en la consulta:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).json({ message: error.message });
     }
 };
 
