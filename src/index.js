86b8423370825c04764b56900aa132a93def1ed7
Victor Raul <v1c70rotakmer@gmail.com>
2024-02-21 11:21:56 -0500

    feat:(auth):refactor docs  and add token for the register user

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import { PORT } from './config/envs.js';
 import { swaggerDocs } from './doc.js';
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on localhost:${PORT} 🚀`);
     swaggerDocs(app);
 });
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -15,52 +15,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/register', register);
-/**
- * @openapi
- *   /api/auth/login:
- *     post:
- *       summary: Login a user
- *       responses:
- *         200:
- *           description: User logged in successfully
- *       requestBody:
- *         required: true
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 email:
- *                   type: string
- *                 password:
- *                   type: string
- *       headers:
- *         Set-Cookie:
- *           description: Token
- *           schema:
- *            type: string
- *            required: true
- *
- */
 router.post('/login', login);
-/**
- * @openapi
- *   /api/auth/profile:
- *     get:
- *       summary: Get user profile
- *       responses:
- *          200:
- *            description: User profile
- *            headers:
- *              Authorization:
- *                description: Token
- *                schema:
- *                  type: string
- *                required: true
- *       security:
- *         - bearerAuth: []
- *         - cookieAuth: []
- */
 router.get('/profile', validatorJwt, profile);
 router.get('/logout', logout);
 
