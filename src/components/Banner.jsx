37ed24e2c55e4062c20cd8af1402c8bd03dd5e43
oscar muñoz <oscarmunoz03@gmail.com>
2024-02-21 22:18:48 -0500

    navbar fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,18 +2,30 @@ import React, { useState } from 'react';
 
 const Banner = () => {
 	return (
-        <div className="hero min-h-screen" style={{backgroundImage: '/s13-23-n-react-back/src/assets/banner.jpg'}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Simplifica la gestión deportiva con SportPlay</h1>
-      
-      <button className="btn Accent">Registrarse</button>
-    </div>
-  </div>
-</div>
-
-    );
+		<div
+			className='hero min-h-screen'
+			style={{
+				backgroundImage: `url('/images/banner.png')`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+			}}
+		>
+			<div className='hero-overlay bg-opacity-5'></div>
+			<div className='hero-content text-neutral-content'>
+				<div className='max-w-md pl-84'>
+					<h1 className='mb-12 text-5xl font-bold'>
+						Simplifica la gestión deportiva con SportPlay
+					</h1>
+					<button className='btn btn-success text-secondary w-167 h-43 mb-14'>
+						Registrarse
+					</button>
+					<button className='btn btn-#1F1D2B text-base-100 w-167 h-43'>
+						Crear Torneo
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Banner;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,82 +1,135 @@
 import React, { useState } from 'react';
+import { FiMenu, FiUser, FiX } from 'react-icons/fi';
 
-const Header = () => {
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
+	const handleModalContainerClick = e => e.stopPropagation();
+
+	const toggleDropdown = () => {
+		setIsDropdownOpen(!isDropdownOpen);
+	};
+
+	const closeMenu = () => {
+		setIsOpen(false);
+	};
+
 	return (
-		<div className='navbar bg-primary'>
-			<div className='navbar-start'>
-				<div className='dropdown'>
-					<div tabIndex={0} role='button' className='btn btn-ghost btn-circle'>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							className='h-5 w-5'
-							fill='none'
-							viewBox='0 0 24 24'
-							stroke='currentColor'
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth='2'
-								d='M4 6h16M4 12h16M4 18h7'
-							/>
-						</svg>
-					</div>
-					<ul
-						tabIndex={0}
-						className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+		<nav className='bg-primary text-white p-4 flex justify-between items-center sticky top-0 z-10 '>
+			{/* Menú hamburguesa */}
+			<div className='flex items-center'>
+				<div className='sm:hidden'>
+					<button
+						onClick={toggleMenu}
+						className='text-white focus:outline-none'
 					>
-						<li>
-							<a>Torneos Abiertos</a>
-						</li>
-						<li>
-							<a>Resultados</a>
-						</li>
-						<li>
-							<a>Equipos</a>
-						</li>
-					</ul>
+						<FiMenu className='h-6 w-6' />
+					</button>
+					{/* Menú hamburguesa desplegable */}
+					{isOpen && (
+						<div
+							onClick={toggleMenu}
+							className='absolute top-0 left-0 w-full h-screen bg-black bg-opacity-50 z-50 flex justify-start items-center '
+						>
+							<div
+								onClick={handleModalContainerClick}
+								className='absolute top-[90px] bg-secondary text-white w-1/2 h-[423px]   '
+							>
+								<ul className='text-white font-Roboto flex flex-col items-start pt-[47px]  '>
+									<li className='py-2 hover:bg-gray-700 w-full pl-[30px] h-[47px]'>
+										{' '}
+										<a>Inicio</a>
+									</li>
+									<li className='py-2 hover:bg-gray-700'>Torneos Abiertos</li>
+									<li className='py-2 hover:bg-gray-700'>Resultados Torneos</li>
+									<li className='py-2 hover:bg-gray-700'>Partidos</li>
+									<li className='py-2 hover:bg-gray-700'>Equipo</li>
+								</ul>
+							</div>
+						</div>
+					)}
 				</div>
-			</div>
-			<div className='navbar-center'>
-				<img
-					src='/images/logo-hori-2.png'
-					className='w-auto h-8'
-				></img>
+
+				{/* Logo */}
+				<div className='flex items-center justify-center sm:justify-start w-64'>
+					<img
+						src='/images/logo-hori-2.png'
+						alt='Logo'
+						className='h-14 sm:h-16 mr-4'
+					/>
+				</div>
 			</div>
 
-			<div className='dropdown dropdown-end'>
-				<div
-					tabIndex={0}
-					role='button'
-					className='btn btn-ghost btn-circle avatar'
-				>
-					<div className='w-10 rounded-full'>
-						<img
-							alt='Tailwind CSS Navbar component'
-							src='https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg'
-						/>
-					</div>
-				</div>
-				<ul
-					tabIndex={0}
-					className='mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52'
-				>
-					<li>
-						<a className='justify-between'>
-							Perfil
-							<span className='badge'>New</span>
-						</a>
-					</li>
-					<li>
-						<a>Actividad</a>
-					</li>
-					<li>
-						<a>Logout</a>
-					</li>
-				</ul>
+			{/* Menú de navegación para pantallas grandes */}
+			<div className='hidden sm:flex space-x-4'>
+				<a href='#' className='hover:text-gray-400'>
+					Inicio
+				</a>
+				<a href='#' className='hover:text-gray-400'>
+					Torneos Abiertos
+				</a>
+				<a href='#' className='hover:text-gray-400'>
+					Resultados Torneos
+				</a>
+				<a href='#' className='hover:text-gray-400'>
+					Partidos
+				</a>
+				<a href='#' className='hover:text-gray-400'>
+					Equipo
+				</a>
 			</div>
-		</div>
+
+			{/* Icono de usuario y menú desplegable */}
+			<div className='relative'>
+				<button
+					onClick={toggleDropdown}
+					className='text-white focus:outline-none'
+				>
+					<FiUser className='h-6 w-6' />
+				</button>
+				{/* Dropdown para el menú de usuario */}
+				{isDropdownOpen && (
+					<div className='absolute right-0 mt-2 w-48 bg-gray-800 rounded-lg shadow-lg'>
+						<ul className='py-2'>
+							<li className='px-4 py-2 hover:bg-gray-700'>
+								<a href='#'>Perfil</a>
+							</li>
+							<li className='px-4 py-2 hover:bg-gray-700'>
+								<a href='#'>Actividad</a>
+							</li>
+							<li className='px-4 py-2 hover:bg-gray-700'>
+								<a href='#'>Torneos</a>
+							</li>
+							<li className='px-4 py-2 hover:bg-gray-700'>
+								<a href='#'>Jugador</a>
+							</li>
+							<li className='px-4 py-2 hover:bg-gray-700'>
+								<a href='#'>Resultados</a>
+							</li>
+							<li className='px-4 py-2 hover:bg-gray-700'>
+								<a href='#'>Notificaciones</a>
+							</li>
+							<li className='px-4 py-2 hover:bg-gray-700'>
+								<a href='#'>Cerrar Sesión</a>
+							</li>
+						</ul>
+					</div>
+				)}
+			</div>
+
+			{/* Overlay para cerrar el menú al hacer clic fuera */}
+			{isOpen && (
+				<div
+					className='fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-40'
+					onClick={closeMenu}
+				></div>
+			)}
+		</nav>
 	);
 };
 
-export default Header;
+export default Navbar;
