0e740e4fe9c6571f8abac8eef7e5bb81a28767f7
Jareth Guerrero <yaredguerrero@gmail.com>
2024-02-24 20:25:58 -0600

    Funciones añadidas para crear, obtener y cerrar torneos.

--- src/controllers/tournament.controller.js
+++ src/controllers/tournament.controller.js
@@ -1,11 +1,11 @@
-import { newTournamentService, getAllTournamentsService, getTournamentsByUserIdService  } from '../services/tournament.services.js'
+import { newTournamentService, getAllTournamentsService, getTournamentsByUserIdService, closeTournamentService } from '../services/tournament.services.js'
 
 
 export const createTournament = async (req, res) => {
     try {
         const tournamentData = req.body;
         const creatorId = req.body.creator_id;
-        const tournamentImage = req.files?.logo; 
+        const tournamentImage = req.files?.logo;
         await newTournamentService(tournamentData, tournamentImage, creatorId);
 
         res.status(201).json({ message: 'Torneo creado exitosamente' });
@@ -15,11 +15,10 @@ export const createTournament = async (req, res) => {
     }
 };
 
-
-
-export const getAllTournaments = async (req, res) => {
+export const getAllTournaments = async (res) => {
     try {
-        const tournaments = await getAllTournamentsService();
+        const tournamentsData = await getAllTournamentsService();
+        const tournaments = tournamentsData[0]
         res.json(tournaments);
     } catch (error) {
         console.error('Error al obtener los torneos:', error);
@@ -29,11 +28,26 @@ export const getAllTournaments = async (req, res) => {
 
 export const getTournamentsByUserId = async (req, res) => {
     try {
-        const userId = req.params.userId;
-        const tournaments = await getTournamentsByUserIdService(userId);
+        const userId = req.query.id;
+        const tournamentsData = await getTournamentsByUserIdService(userId);
+        const tournaments = tournamentsData[0]
+
         res.json(tournaments);
     } catch (error) {
         console.error('Error al obtener los torneos del usuario:', error);
         res.status(500).json({ error: 'Error interno del servidor' });
     }
 };
+
+export const closeTournament = async (req, res) => {
+    try {
+        const tournamentId = req.body.id;
+        const userId = req.query.id;
+        await closeTournamentService(tournamentId, userId);
+
+        res.status(201).json({ message: "Torneo cerrado de forma exitosa" });
+    } catch (error) {
+        console.error('Error al cerrar el torneo:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+}
