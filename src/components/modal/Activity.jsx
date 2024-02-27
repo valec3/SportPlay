3472623ff695bc17c8233682b8037ec7ddb37a1f
Miguel Evangelista <miguelbranagan30@gmail.com>
2024-02-27 16:38:24 -0400

    page activity done

--- src/components/modal/Activity.jsx
+++ src/components/modal/Activity.jsx
@@ -1,10 +1,82 @@
+const Tournament = ({ logo, name, role }) => (
+	<div className='w-full bg-secondary rounded-[14px] px-5 flex items-center justify-between py-[13px]'>
+		<div className='flex items-center gap-[6px]'>
+			<img src={logo} alt='Logo' />
+			<span className='text-xl font-semibold font-SourceSansPro'>{name}</span>
+		</div>
+		<span className='text-accent font-SourceSansPro leading-[19.2px]'>
+			{role}
+		</span>
+	</div>
+);
+
+const Match = ({
+	nameTeamOne,
+	nameTeamTwo,
+	date,
+	logoTeamOne,
+	logoTeamTwo,
+}) => (
+	<div className='w-full bg-secondary rounded-[14px] px-3 py-4 flex items-center gap-[14px]'>
+		<div className='flex items-center gap-[9px]'>
+			<span className='font-SourceSansPro'>{nameTeamOne}</span>
+			<img src={logoTeamOne} alt='Logo' />
+		</div>
+		<span className='font-SourceSansPro text-warning'>{date}</span>
+		<div className='flex items-center gap-[9px] flex-row-reverse'>
+			<span className='font-SourceSansPro'>{nameTeamTwo}</span>
+			<img src={logoTeamTwo} alt='Logo' />
+		</div>
+	</div>
+);
+
 export default function Activity() {
+	// Data for active tournaments
+	const activeTournaments = [
+		{
+			name: 'Copa Las Condes',
+			role: 'Administrador',
+			logo: 'images/AdminCopaLogo.png',
+		},
+	];
+
+	const activePlayerTournaments = [
+		{ name: 'Torneo A', role: 'Jugador', logo: 'images/cup.png' },
+	];
+
+	const nextAdminTournament = [
+		{
+			nameTeamOne: 'Equipo “A”',
+			nameTeamTwo: 'Equipo “B”',
+			date: '2024-10-30',
+			logoTeamOne: 'images/TeamAdminTorneoLogo.png',
+			logoTeamTwo: 'images/TeamAdminTorneoLogo.png',
+		},
+
+		{
+			nameTeamOne: 'Equipo “C”',
+			nameTeamTwo: 'Equipo “D”',
+			date: '2024-09-10',
+			logoTeamOne: 'images/TeamAdminTorneoLogo.png',
+			logoTeamTwo: 'images/TeamAdminTorneoLogo.png',
+		},
+	];
+
+	const nextPlayerTournament = [
+		{
+			nameTeamOne: 'Man City',
+			nameTeamTwo: 'Crystal Pa',
+			date: '2024-07-29',
+			logoTeamOne: 'images/atletico.png',
+			logoTeamTwo: 'images/albion.png',
+		},
+	];
+
 	return (
 		<section className='pt-[108px] pb-[119px] bg-primary text-base-100'>
 			<div className='px-[30px]'>
-				<h1 className=' font-Roboto font-bold text-[32px]'>Actividad</h1>
-
-				<div className=' flex justify-between mt-[43px]'>
+				<h1 className='font-Roboto font-bold text-[32px]'>Actividad</h1>
+				<div className='flex justify-between mt-[43px]'>
 					<h2 className='font-bold text-[22px] font-Roboto leading-[24.53px] tracking-[0.263px]'>
 						Torneos Activos
 					</h2>
@@ -14,163 +86,57 @@ export default function Activity() {
 				</div>
 			</div>
 
-			<div className=' px-[30px] flex flex-col gap-3'>
-				<div className='w-full mt-[33px] bg-secondary px-5 flex items-center justify-between py-[13px] rounded-[14px]'>
-					<div className='flex items-center gap-[6px]'>
-						<img src='images/AdminCopaLogo.png' alt='Cup' />
-						<span className='text-xl font-semibold font-SourceSansPro'>
-							Copa Las Condes
-						</span>
-					</div>
-					<span className=' text-accent font-SourceSansPro leading-[19.2px]'>
-						Administrador
-					</span>
+			<div className=' mt-[25px] flex flex-col gap-3'>
+				<div className='px-[30px] flex flex-col gap-3'>
+					{activeTournaments.map((tournament, index) => (
+						<Tournament key={index} {...tournament} />
+					))}
 				</div>
 
-				<div className='w-full bg-secondary px-5 flex items-center justify-between py-[13px] rounded-[14px]'>
-					<div className='flex items-center gap-[6px]'>
-						<img src='images/cup.png' alt='Cup' />
-						<span className='text-xl font-semibold font-SourceSansPro'>
-							Torneo “A”
-						</span>
-					</div>
-					<span className=' text-accent font-SourceSansPro leading-[19.2px]'>
-						Jugador
-					</span>
+				<div className='px-[30px] flex flex-col gap-3'>
+					{activePlayerTournaments.map((tournament, index) => (
+						<Tournament key={index} {...tournament} />
+					))}
 				</div>
 			</div>
 
-			<img className=' mt-[102px]' src='images/divider.svg' alt='Divider' />
+			<img className='mt-[102px]' src='images/divider.svg' alt='Divider' />
 
 			<div className='px-[30px] mt-[56.89px]'>
-				<h3 className=' font-Roboto font-medium text-[22px] leading-[24.53px] tracking-[0.263px]'>
+				<h3 className='font-Roboto font-medium text-[22px] leading-[24.53px] tracking-[0.263px]'>
 					Próximo Encuentro
 				</h3>
 
-				<div>
-					<div className='hidden '>
-						<p className=' mt-[61px] text-center text-warning font-Roboto text-xl'>
-							Los partidos aún no tienen fecha de planificación. Mantente atento
-							para futuras actualizaciones
-						</p>
+				<div className=' mt-9'>
+					<div className='flex items-center gap-[5px]'>
+						<img src={activeTournaments[0].logo} alt='Logo' />
+						<span className='text-lg font-semibold font-Roboto'>
+							{activeTournaments[0].name}
+						</span>
 					</div>
 
-					<div className=' mt-9'>
-						<div>
-							<div className='flex items-center gap-[5px]'>
-								<img src='images/AdminCopaLogo.png' alt='Admin Copa Logo' />
-								<h4 className='text-lg font-Roboto'>Copa Las Condes</h4>
-							</div>
-
-							<div className='mt-6 flex flex-col gap-[10px]'>
-								<div className='w-full bg-secondary rounded-[14px] px-3 py-4 flex items-center gap-[14px]'>
-									<div className='flex items-center gap-[9px]'>
-										<span className=' font-SourceSansPro'>Equipo “A”</span>
-										<img
-											src='images/TeamAdminTorneoLogo.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-									<span className=' font-SourceSansPro text-warning'>
-										27 Agos 2024
-									</span>
-									<div className='flex items-center gap-[9px] flex-row-reverse'>
-										<span className=' font-SourceSansPro'>Equipo “B”</span>
-										<img
-											src='images/TeamAdminTorneoLogo.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-								</div>
-								<div className='w-full bg-secondary rounded-[14px] px-3 py-4 flex items-center gap-[14px]'>
-									<div className='flex items-center gap-[9px]'>
-										<span className=' font-SourceSansPro'>Equipo “C”</span>
-										<img
-											src='images/TeamAdminTorneoLogo.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-									<span className=' font-SourceSansPro text-warning'>
-										12 Dic 2024
-									</span>
-									<div className='flex items-center gap-[9px] flex-row-reverse'>
-										<span className=' font-SourceSansPro'>Equipo “D”</span>
-										<img
-											src='images/TeamAdminTorneoLogo.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-								</div>
-								<div className='w-full bg-secondary rounded-[14px] px-3 py-4 flex items-center gap-[14px]'>
-									<div className='flex items-center gap-[9px]'>
-										<span className=' font-SourceSansPro'>Equipo “E”</span>
-										<img
-											src='images/TeamAdminTorneoLogo.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-									<span className=' font-SourceSansPro text-warning'>
-										12 Dic 2024
-									</span>
-									<div className='flex items-center gap-[9px] flex-row-reverse'>
-										<span className=' font-SourceSansPro'>Equipo “F”</span>
-										<img
-											src='images/TeamAdminTorneoLogo.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-								</div>
-								<div className='w-full bg-secondary rounded-[14px] px-3 py-4 flex items-center gap-[14px]'>
-									<div className='flex items-center gap-[9px]'>
-										<span className=' font-SourceSansPro'>Equipo “G”</span>
-										<img
-											src='images/TeamAdminTorneoLogo.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-									<span className=' font-SourceSansPro text-warning'>
-										12 Dic 2024
-									</span>
-									<div className='flex items-center gap-[9px] flex-row-reverse'>
-										<span className=' font-SourceSansPro'>Equipo “H”</span>
-										<img
-											src='images/TeamAdminTorneoLogo.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-								</div>
-							</div>
+					<div className=' mt-[25px] flex flex-col gap-3'>
+						<div className='flex flex-col gap-3'>
+							{nextAdminTournament.map((adminTournament, index) => (
+								<Match key={index} {...adminTournament} />
+							))}
 						</div>
+					</div>
+				</div>
 
-						<div className=' mt-[77px]'>
-							<div className='flex items-center gap-[5px]'>
-								<img
-									src='images/TorneoPlayerLogo.png'
-									alt='Torneo Player Logo'
-								/>
-								<h4 className='text-lg font-Roboto text-base-100'>
-									Torneo “A”
-								</h4>
-							</div>
+				<div className=' mt-[68px]'>
+					<div className='flex items-center gap-[5px]'>
+						<img src={activePlayerTournaments[0].logo} alt='Logo' />
+						<span className='text-lg font-semibold font-Roboto'>
+							{activePlayerTournaments[0].name}
+						</span>
+					</div>
 
-							<div className='mt-6 flex flex-col gap-[10px]'>
-								<div className='w-full bg-secondary rounded-[14px] px-3 py-4 flex items-center gap-[14px]'>
-									<div className='flex items-center gap-[9px]'>
-										<span className=' font-SourceSansPro'>Man City</span>
-										<img
-											src='images/Atletico.png'
-											alt='Team Admin Torneo Logo'
-										/>
-									</div>
-									<span className=' font-SourceSansPro text-warning'>
-										29 Jul 2024
-									</span>
-									<div className='flex items-center gap-[9px] flex-row-reverse'>
-										<span className=' font-SourceSansPro'>Crystal Pa</span>
-										<img src='images/albion.png' alt='Team Admin Torneo Logo' />
-									</div>
-								</div>
-							</div>
+					<div className=' mt-[25px] flex flex-col gap-3'>
+						<div className='flex flex-col gap-3'>
+							{nextPlayerTournament.map((playerTournament, index) => (
+								<Match key={index} {...playerTournament} />
+							))}
 						</div>
 					</div>
 				</div>
