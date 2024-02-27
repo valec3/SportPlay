9f5072787cd40ba9763adb7747c6d541dde674f0
Miguel Evangelista <miguelbranagan30@gmail.com>
2024-02-27 16:40:26 -0400

    activity added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 
 import Footer from './components/Footer';
-import Activity from './components/modal/Activity';
 
 function App() {
 	return (
 		<>
-			<Activity />
+			<div className='flex flex-col min-h-screen bg-primary text-base-100'>
+				{/* header */}
+
+				<main className=''>
+					<BrowserRouter>
+						<Routes>
+							<Route path='/' element={<Home />} />
+						</Routes>
+					</BrowserRouter>
+
+					{/* <main className=' px-[30px] space-y-4 text-base-100 md:px-[45px] min-h-screen'> */}
+				</main>
+				{/* footer */}
+				<Footer />
+			</div>
 		</>
 	);
 }
