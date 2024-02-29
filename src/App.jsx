413e9ec4a112b0a50e872491c4b594e8b9060932
Miguel Evangelista <miguelbranagan30@gmail.com>
2024-02-29 15:53:33 -0400

    adding the app component code for the add player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import Addplayer from './modal/AddPlayer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<>
 			<div className='flex flex-col min-h-screen bg-primary text-base-100'>
-				<Addplayer />
+				{/* header */}
+
+				<main className=''>
+					<BrowserRouter>
+						<Routes>
+							<Route path='/' element={<Home />} />
+						</Routes>
+					</BrowserRouter>
+
+					{/* <main className=' px-[30px] space-y-4 text-base-100 md:px-[45px] min-h-screen'> */}
+				</main>
+				{/* footer */}
+				<Footer />
 			</div>
 		</>
 	);
