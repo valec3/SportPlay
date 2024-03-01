27e7622be68652405f08770c3d0785f29d8998c7
madays <maday.choque@gmail.com>
2024-03-01 15:43:20 -0500

    tournament admin page

--- src/pages/administrarTorneo/AdminTorneo.jsx
+++ src/pages/administrarTorneo/AdminTorneo.jsx
@@ -1,45 +1,42 @@
 import { Link } from 'react-router-dom';
 import { FaAngleDown } from 'react-icons/fa6';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function AdminTorneo() {
 	const currentTournament = (
-		<div className='z-[1] '>
-			<div className='dropdown dropdown-end'>
-				<div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
-					<FaAngleDown />
-				</div>
-				<ul
-					tabIndex={0}
-					className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-secondary rounded-box w-52'
-				>
-					<li>
-						<Link className='text-right block'>Compartir Administración</Link>
-					</li>
-					<li>
-						<Link to={'/'} className='text-right block'>
-							Administrar Equipos
-						</Link>
-					</li>
-					<li>
-						<Link className='text-right block'>Organizar Partidos</Link>
-					</li>
-					<li>
-						<Link className='text-right block'>Cargar Resultados</Link>
-					</li>
-					<li className=''>
-						<Link className='text-right block'>Notificaciones</Link>
-					</li>
-					<li>
-						<Link className='text-warning text-right block   '>
-							<div className='flex flex-row  items-center justify-end'>
-								<img src='/icons/exit.svg' alt='' />
-								<div className='ml-2'>Finalizar Torneo</div>
-							</div>
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</div>
+		<details className='dropdown dropdown-end bg-secondary'>
+			<summary className='m-1 btn bg-secondary border-secondary'>
+				<FaAngleDown className='text-base-100 ' />
+			</summary>
+			<ul className='p-2 shadow menu dropdown-content z-[1] bg-secondary rounded-box w-52'>
+				<li className=''>
+					<Link className='text-right block'>Compartir Administración</Link>
+				</li>
+				<li>
+					<Link to={'/'} className='text-right block'>
+						Administrar Equipos
+					</Link>
+				</li>
+				<li>
+					<Link className='text-right block'>Organizar Partidos</Link>
+				</li>
+				<li>
+					<Link className='text-right block'>Cargar Resultados</Link>
+				</li>
+				<li className=''>
+					<Link className='text-right block'>Notificaciones</Link>
+				</li>
+				<li>
+					<Link className='text-warning text-right block   '>
+						<div className='flex flex-row  items-center justify-end'>
+							<img src='/icons/exit.svg' alt='' />
+							<div className='ml-2'>Finalizar Torneo</div>
+						</div>
+					</Link>
+				</li>
+			</ul>
+		</details>
 	);
 	const tournamentCancel = (
 		<img src='/icons/iconCancel.svg' alt='icono de calcelar' />
@@ -47,6 +44,23 @@ function AdminTorneo() {
 	const wonTournament = (
 		<img src='/icons/trophyGreen.svg' alt='icono de calcelar' />
 	);
+	const [tournaments, setTournaments] = useState([]); // State to store fetched data
+	const [error, setError] = useState(null); // State to store any errors
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(
+					'https://tournament-sport.onrender.com/api/tournament/all-tournaments'
+				);
+				setTournaments(response.data);
+			} catch (error) {
+				setError(error);
+			}
+		};
+
+		fetchData();
+	}, [tournaments]);
 	return (
 		<div className='space-y-4'>
 			<div className='m-auto w-[327px] md:w-[400px] space-y-4 text-neutral'>
@@ -65,17 +79,30 @@ function AdminTorneo() {
 			<div className='bg-[#545458] w-full h-[0.5px] mt-0'></div>
 			<div className='m-auto w-[327px] md:w-[400px] space-y-4 text-neutral'>
 				<h2>Torneos creados</h2>
-				<div className='flex flex-row items-center justify-between bg-secondary h-[61px] p-4 rounded-[14px]'>
-					<div className='flex flex-row gap-4 items-center '>
-						<img
-							src='/images/trophyAdminTournament.svg'
-							alt='icono de trofeo'
-							className=''
-						/>
-						<div className='w-full'>Copa las Condes</div>
-					</div>
-					{/* {currentTournament} */}
-				</div>
+				{tournaments.map((tournament, index) => {
+					return (
+						<div
+							className='flex flex-row items-center justify-between bg-secondary h-[61px] p-4 rounded-[14px]'
+							key={index}
+						>
+							<div className='flex flex-row gap-4 items-center '>
+								<img
+									src={
+										tournament?.logo
+											? tournament.logo
+											: '/icons/trophyAdminTournament.svg'
+									}
+									alt='icono de trofeo'
+									className='w-[40px] h-[40px] rounded-full'
+								/>
+								<div className='w-full'>{tournament.name}</div>
+							</div>
+							{tournament.status == 'ongoing' ? currentTournament : ''}
+							{tournament.status == 'finished' ? wonTournament : ''}
+							{tournament.status == 'cancelled' ? tournamentCancel : ''}
+						</div>
+					);
+				})}
 			</div>
 		</div>
 	);
