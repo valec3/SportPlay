48943c5925dbd96ec9ac54bd4dd8ac65db54cb7c
madays <maday.choque@gmail.com>
2024-03-07 14:12:22 -0500

    avances

--- src/pages/partidos/Partidos.jsx
+++ src/pages/partidos/Partidos.jsx
@@ -3,57 +3,33 @@ import { useState, useEffect } from 'react';
 
 const Partidos = () => {
 	const [partidos, setPartidos] = useState([]);
-	const [torneo, setTorneo] = useState('');
+	const [torneos, setTorneos] = useState([]);
 
 	useEffect(() => {
 		const fetchPartidosData = async () => {
-			const mockData = [
-				{
-					torneo: 'Torneo A',
-					logo: 'images/barcelona.svg',
-					equipoLocal: {
-						id: 1,
-						nombre: 'Atlético M.',
-						logo: 'images/atletico.svg',
-					},
-					equipoVisitante: {
-						id: 2,
-						nombre: 'Villarreal',
-						logo: 'images/villareal.svg',
-					},
-					date: '15 mar 2024',
-				},
-				{
-					torneo: 'Torneo B',
-					logo: 'images/barcelona.svg',
-					equipoLocal: {
-						id: 1,
-						nombre: 'Atlético M.',
-						logo: 'images/atletico.svg',
-					},
-					equipoVisitante: {
-						id: 2,
-						nombre: 'Villarreal',
-						logo: 'images/villareal.svg',
-					},
-					date: '15 mar 2024',
-				},
-				// Otras definiciones de partidos
-			];
-
-			setPartidos(mockData);
-			setTorneo(mockData[0].torneo);
-
 			const response = await axios.get(
 				`https://tournament-sport.onrender.com/api/games/info-list`
 			);
 			const partidos = await response.data;
-			console.log(partidos.data);
-			// setPartidos(partidos.data);
+			const partidosInfo = partidos.data;
+
+			const torneos = Object.values(
+				partidosInfo.reduce((partidos, partido) => {
+					const torneo = partido.tournament_name;
+					partidos[torneo] = partidos[torneo] || [];
+					partidos[torneo].push(partido);
+					return partidos;
+				}, {})
+			);
+
+			setPartidos(partidosInfo);
+			setTorneos(torneos);
 		};
 
 		fetchPartidosData();
 	}, []);
+	console.log('Torneos', torneos);
+	console.log('partidos', partidos);
 
 	return (
 		<>
@@ -62,56 +38,56 @@ const Partidos = () => {
 					Partidos
 				</h1>
 			</div>
+			{}
 			<div className='ml-12 mb-5 text-SansPro font-semibold'>
 				<img
-					src={partidos[0]?.logo}
-					alt={torneo}
+					src={torneos[0][0].tournament_logo}
+					alt='logo del torneo'
 					className='w-10 h-10 mr-3 inline-block'
 				/>
-				{torneo}
+				{torneos[0][0].tournament_name}
 			</div>
-			{partidos.map((partido, index) => (
-				<div key={index} className='ml-12 mb-5 text-SansPro font-semibold'>
-					{partido.torneo !== torneo && (
-						<>
+			{torneos[0].map((partido, index) => {
+				return (
+					<div key={index}>
+						{/* <div className='ml-12 mb-5 text-SansPro font-semibold'>
 							<img
-								src={partido.logo}
-								alt={partido.torneo}
+								src={partido.tournament_logo}
+								alt='logo del torneo'
 								className='w-10 h-10 mr-3 inline-block'
 							/>
-							{partido.torneo}
-						</>
-					)}
-					<div
-						key={index}
-						className='bg-secondary rounded-[14px] h-45 w-335 ml-0 mx-6 my-6 overflow-hidden drop-shadow-lg flex items-center justify-center'
-					>
-						<div className='p-1 pt-1 w-full mr-2 bg-secondary rounded-[14px]'>
-							<div className='h-[60px] flex ml-2 mr-4 items-center justify-center'>
-								<div className='flex items-center'>
-									<span>{partido.equipoLocal.nombre}</span>
-									<img
-										className='w-10 h-10 ml-2'
-										src={partido.equipoLocal.logo}
-										alt={partido.equipoLocal.nombre}
-									/>
-								</div>
-								<div className='text-warning flex items-center justify-center ml-5 '>
-									{partido.date}
-								</div>
-								<div className='flex items-center '>
-									<img
-										className='w-10 h-10 ml-2'
-										src={partido.equipoVisitante.logo}
-										alt={partido.equipoVisitante.nombre}
-									/>
-									<span>{partido.equipoVisitante.nombre}</span>
+							{partido.tournament_name}
+						</div> */}
+						<div className='ml-12 mb-5 text-SansPro font-semibold'>
+							<div className='bg-secondary rounded-[14px] h-45 w-335 ml-0 mx-6 my-6 overflow-hidden drop-shadow-lg flex items-center justify-center'>
+								<div className='p-1 pt-1 w-full mr-2 bg-secondary rounded-[14px]'>
+									<div className='h-[60px] flex ml-2 mr-4 items-center justify-center'>
+										<div className='flex items-center'>
+											<span>{partido.home_team_name}</span>
+											<img
+												className='w-10 h-10 ml-2'
+												src={partido.home_team_logo}
+												alt={partido.home_team_name}
+											/>
+										</div>
+										<div className='text-warning flex items-center justify-center ml-5 '>
+											fecha
+										</div>
+										<div className='flex items-center '>
+											<img
+												className='w-10 h-10 ml-2'
+												src={partido.away_team_logo}
+												alt={partido.away_team_name}
+											/>
+											<span>{partido.away_team_name}</span>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			))}
+				);
+			})}
 		</>
 	);
 };
